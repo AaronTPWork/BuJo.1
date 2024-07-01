700a4c3ff29d5747090dc065c15004127a90df98
AaronTPWork <160541322+AaronTPWork@users.noreply.github.com>
2024-07-01 11:38:14 -0400

    Revert "Made due_date component && used"

--- src/Pages/Home/sections/components/DynamicFloatingMenu.js
+++ src/Pages/Home/sections/components/DynamicFloatingMenu.js
@@ -36,12 +36,6 @@ import {
 } from '../../../../Components/icons';
 import { useBulletIcons } from '../../../../Services/Reference/hooks';
 import DelegateModal from './DelegateModal';
-import { changeDueDate } from '../../../../Services/Note/api';
-import DatePicker from 'react-datepicker';
-import moment from 'moment-timezone';
-import 'react-datepicker/dist/react-datepicker.css';
-
-import './custom-datepicker.css';
 
 const iconComponents = {
   'bullet-init-note': InitNote,
@@ -92,24 +86,13 @@ export const DynamicFloatingMenu = ({
   selectedIcon,
   getIconName,
   note,
-  updateDate,
 }) => {
   const { data } = useBulletIcons(selectedIcon);
-  const [ref, setRef] = useState();
-  const [dueDate, setDueDate] = useState('');
-  const [isOpened, setisOpened] = useState(false);
-  const handleDelegate = (email) =>
-    !isOpened && selectIcon({ iconId: ref.id, del_email: email }, ref, isOpened);
 
-  const handleDueDate = async (e) => {
-    try {
-      await changeDueDate(e, note);
-      const formattedDate = e ? moment(e).tz('America/New_York').toDate() : '';
-      setDueDate(formattedDate);
-      updateDate();
-    } catch (e) {
-      console.error(e);
-    }
+  const [ref, setRef] = useState();
+
+  const handleDelegate = (email) => {
+    selectIcon({ iconId: ref.id, del_email: email }, ref);
   };
 
   return (
@@ -127,7 +110,7 @@ export const DynamicFloatingMenu = ({
         <p className="pb-2 mx-auto text-sm">ADD A TAG</p>
         <div className="flex flex-col items-start mt-2 overflow-scroll">
           {data &&
-            data?.length &&
+            data?.length > 0 &&
             data?.map((ref, idx) => {
               if (getIconName(ref) === 'rectangle-init-image') {
                 return (
@@ -187,25 +170,7 @@ export const DynamicFloatingMenu = ({
                   className="icon_button"
                 >
                   {getIconComponent(getIconName(ref), 'h-4')}
-                  {ref?.state !== 'init' ? (
-                    <span className="pl-2 text-left">{getIconName(ref)}</span>
-                  ) : (
-                    <DatePicker
-                      selected={dueDate}
-                      className="bg-transparent ml-1.5 w-full cursor-pointer text-black placeholder-black"
-                      placeholderText="diamond-init-appointment"
-                      onChange={handleDueDate}
-                      timeFormat="HH:mm"
-                      dateFormat="yyyy-MM-dd HH:mm:ss"
-                      showTimeSelect
-                      showTimeSelectOnly={false}
-                      timeIntervals={15}
-                      timeCaption="Time"
-                      onCalendarOpen={() => setisOpened(true)}
-                      onCalendarClose={() => setisOpened(false)}
-                      calendarClassName="relative"
-                    />
-                  )}
+                  <span className="pl-2 text-left">{getIconName(ref)}</span>
                 </button>
               );
             })}
